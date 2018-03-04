71339d6a9fbea4c10f15417ade8e5ae3fbd73b0a
porkstart <11462748+porkstart@users.noreply.github.com>
2018-03-04 09:45:10 +0000

    Added dry and logging for other commands as well

--- server/commands/create.command.js
+++ server/commands/create.command.js
@@ -4,25 +4,24 @@ const UserModel = require("./../models/user.model");
 const { getOrGenImg, makeNapChartImageUrl, createChart } = require("./../imageCache");
 
 module.exports = {
-  processCreate: function(command, message, args) {
-    if (command === "create") {
-      if (args.length >= 1) {
-        create(args, message);
-      } else {
-        //What?
-        message.channel.send(
-          "Type `+create` followed by a series of time ranges. For example, `+create 03:00-05:00 08:00-08:20 14:00-14:20 21:00-23:00`. A napchart link will then be generated for you. (If you want to set it, you will have to do that manually afterwards)"
-        );
-      }
-    }
-  }
+	processCreate: function(command, message, args,dry=false) {
+		if (command === "create") {
+			if (args.length >= 1) {
+				create(args, message, dry);
+			} else {
+				msg = "Type `+create` followed by a series of time ranges. For example, `+create 03:00-05:00 08:00-08:20 14:00-14:20 21:00-23:00`. A napchart link will then be generated for you. (If you want to set it, you will have to do that manually afterwards)"
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			}
+		}
+	}
 };
 
 async function create(args, message) {
-  let timeFrames = args.split(' ');
+	let timeFrames = args.split(' ');
 
-  let data = {};
-  /*[{
+	let data = {};
+	/*[{
 		"start":720,
 		"end": 790,
 		"text": "Cool text"
@@ -30,5 +29,5 @@ async function create(args, message) {
 		"start":1420,
 		"end":400
 	}]*/
-  createChart(data)
+	createChart(data)
 }
--- server/commands/markdown.command.js
+++ server/commands/markdown.command.js
@@ -5,40 +5,41 @@ const { getOrGenImg } = require("./../imageCache");
 let commands = {};
 
 async function bootstrapCommands() {
-  let cmds = await fs.readdirSync("./commands");
-  cmds.forEach(async function(d) {
-    if (!d.endsWith(".md")) return;
+	let cmds = await fs.readdirSync("./commands");
+	cmds.forEach(async function(d) {
+		if (!d.endsWith(".md")) return;
 
-    let commandName = d.replace(".md", "").toLowerCase();
-    let text = await fs.readFileSync("./commands/" + d, "utf8");
-    if (commandName.indexOf(".") >= 0) {
-      commandName = commandName.split(".")[0];
-      if (!commands.hasOwnProperty(commandName)) {
-        commands[commandName] = [];
-      }
-      commands[commandName].push(text);
-    } else {
-      commands[commandName] = text;
-    }
-  });
+		let commandName = d.replace(".md", "").toLowerCase();
+		let text = await fs.readFileSync("./commands/" + d, "utf8");
+		if (commandName.indexOf(".") >= 0) {
+			commandName = commandName.split(".")[0];
+			if (!commands.hasOwnProperty(commandName)) {
+				commands[commandName] = [];
+			}
+			commands[commandName].push(text);
+		} else {
+			commands[commandName] = text;
+		}
+	});
 }
 
 bootstrapCommands();
 
 module.exports = {
-  processMarkdownCommands: function(command, message, args) {
-    if (commands.hasOwnProperty(command)) {
-      let mVal = commands[command];
-      if (Array.isArray(mVal)) {
-        mVal.forEach(m => message.channel.send(m));
-      } else {
-        message.channel.send(mVal);
-      }
+	processMarkdownCommands: function(command, message, args, dry=false) {
+		if (commands.hasOwnProperty(command)) {
+			let mVal = commands[command];
+			console.log("MSG   : ", "[Markdown for " + command + "]")
+			if (Array.isArray(mVal)) {
+				if(!dry){mVal.forEach(m => message.channel.send(m));}
+			} else {
+				if(!dry){message.channel.send(mVal);}
+			}
 
-      if (schedules.hasOwnProperty(command)) {
-        schedules[command].chart &&
-          getOrGenImg(schedules[command].chart, message);
-      }
-    }
-  }
+			if (schedules.hasOwnProperty(command)) {
+				schedules[command].chart &&
+					getOrGenImg(schedules[command].chart, message, dry);
+			}
+		}
+	}
 };

414363adfa681dcd3cef9e998f8e2a8e537e7fa0
porkstart <11462748+porkstart@users.noreply.github.com>
2018-03-06 17:01:31 +0000

    m/set functions now have blocking version

--- server/commands/mset.command.js
+++ server/commands/mset.command.js
@@ -31,7 +31,34 @@ module.exports = {
 			return true
 		}
 		return false
-	}
+	},
+	processMsetBlock: (async function(command, message, args, dry=false) {
+		if (command === "mset") {
+			let roles =  message.member.roles
+			roles = new Set(roles.keys())
+			let mods = message.guild.roles.find("name", "Moderators").id
+			let admins = message.guild.roles.find("name", "Admins").id
+			permissions = false
+			if (roles.has(mods)||roles.has(admins)) {
+				permissions = true
+			}
+			if (!permissions) {
+				msg = "You do not have privileges to execute this commands. Only Moderators and Admins are allowed to use `+mset`"
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			}
+			else if (args.length >= 3) {
+				await mset(args, message, dry);
+			} else {
+				msg = "Valid options are `+mset [schedule-name] [napchart-link] [username]`"
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			}
+			return true
+		}
+		return false
+	})
+
 };
 
 async function mset(args, message, dry) {

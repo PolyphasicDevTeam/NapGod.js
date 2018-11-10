77ff68276977fbfa0097ef5f23bce37101f073d9
jackred <gouzoudorian@gmail.com>
2018-11-10 18:45:32 +0100

    ADD events role dquote> dquote> events role is now toggleable with the <toggleevents> function

---
+++ server/commands/events.command.js
@@ -0,0 +1,32 @@
+const config = require("../../config.json");
+const _ = require("lodash");
+
+module.exports = {
+	processToggleEvents: function(command, message, args, dry=false) {
+		if (command === "toggleevents") {
+			console.log("CMD   : TOGGLEEVENTS")
+			console.log("ARGS  : ", args)
+
+			let roles =  message.member.roles
+			roles = new Set(roles.keys())
+			eventsRole = message.guild.roles.find("name", "Events").id
+			if(roles.has(eventsRole)) {
+				roles.delete(eventsRole)
+				msg = message.author.tag + " no longer has the Events role."
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			} else {
+				roles.add(eventsRole)
+				msg = message.author.tag + " has now the Events role."
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			}
+			if(!dry){message.member.setRoles(Array.from(roles));}
+
+			return true
+		}
+		return false
+	}
+};
+
+

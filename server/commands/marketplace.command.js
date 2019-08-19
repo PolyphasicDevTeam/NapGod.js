c9b274f06d66f3f722baa4afef86ae035d895fba
Ambareesh Balaji <ambareeshbalaji@gmail.com>
2019-08-19 05:43:31 -0700

    Add togglemarketplace command

---
+++ server/commands/marketplace.command.js
@@ -0,0 +1,32 @@
+const config = require("../../config.json");
+const _ = require("lodash");
+
+module.exports = {
+	processToggleMarketplace: function(command, message, args, dry=false) {
+		if (command === "togglemarketplace") {
+			console.log("CMD   : TOGGLEMARKETPLACE")
+			console.log("ARGS  : ", args)
+
+			let roles =  message.member.roles
+			roles = new Set(roles.keys())
+			const marketplaceRole = message.guild.roles.find("name", "Marketplace").id
+			if(roles.has(marketplaceRole)) {
+				roles.delete(marketplaceRole)
+				const msg = message.author.tag + " no longer has the Marketplace role."
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			} else {
+				roles.add(marketplaceRole)
+				const msg = message.author.tag + " has now the Marketplace role."
+				console.log("MSG   : ", msg)
+				if(!dry){message.channel.send(msg);}
+			}
+			if(!dry){message.member.setRoles(Array.from(roles));}
+
+			return true
+		}
+		return false
+	}
+};
+
+

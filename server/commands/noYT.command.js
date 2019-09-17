a39a5e565c45bd44e429e82acee7b67642635657
Jackred <jackred@tuta.io>
2019-09-17 15:53:28 +0100

    ADD noYT command for toggling
    future update will contain a toggle command for all toggleable role instead of multiple copypasta command

---
+++ server/commands/noYT.command.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const config = require("../../config.json");
+
+module.exports = {
+  processToggleEvents: function(command, message, args, dry=false) {
+    if (command === "noyt") {
+      console.log("CMD   : NOYT");
+      console.log("ARGS  : ", args);
+      let msg = "";
+      let roles =  message.member.roles;
+      roles = new Set(roles.keys());
+      let noYTRole = message.guild.roles.find("name", "No YT plz").id;
+      if(roles.has(noYTRole)) {
+	roles.delete(noYTRole);
+	msg = message.author.tag + " no longer has the No YT plz role.";
+	console.log("MSG   : ", msg);
+	if(!dry){message.channel.send(msg);}
+      } else {
+	roles.add(noYTRole);
+	msg = message.author.tag + " has now the No YT plz role.";
+	console.log("MSG   : ", msg);
+	if(!dry){message.channel.send(msg);}
+      }
+      if(!dry){message.member.setRoles(Array.from(roles));}
+
+      return true;
+    }
+    return false;
+  }
+};
+
+

ae4276ed8c7ea8e5a4758cff46bf9c2953a4f1cc
Corentin Giroud-Argoud <giroud_c@epita.fr>
2020-06-01 03:04:38 +0200

    dev/log: log: add qna for FSL and cleanup some

--- server/models/log.model.js
+++ server/models/log.model.js
@@ -3,17 +3,32 @@ const mongoose = require('mongoose');
 let schema = {
   userName: { type: String, required: true },
   schedule: { type: String, required: true},
-  day: {type: Number, required: true},
   attempt: {type: Number, required: true},
-  daySegments: {type: String, required: true},
-  moods: {type: String, required: true},
-  awakeDifficulty: {type: Number, required: true},
-  sleepTime: {type: Number, required: true},
-  oversleepTime: {type: Number},
-  napsNumber: {type: Number},
-  logMessage: {type: String},
-  attachment: {type: String},
-  loggedAt: {type: Date, default: Date.now}
+
+  monoSleep: {type: String},
+  experience: {type: String},
+  previousFeeling: {type: String},
+  sleepDep: {type: String},
+  previousSched: {type: String},
+  monoDep: {type: String},
+  polyDep: {type: String},
+  polyNDep: {type: String},
+  reasonChange: {type: String},
+
+  entries: [
+    {
+      day: {type: Number, required: true},
+      daySegments: {type: String, required: true},
+      moods: {type: String, required: true},
+      awakeDifficulty: {type: Number, required: true},
+      sleepTime: {type: Number, required: true},
+      oversleepTime: {type: Number},
+      napsNumber: {type: Number},
+      logMessage: {type: String},
+      attachment: {type: String},
+      loggedAt: {type: Date, default: Date.now}
+    }
+  ]
 };
 
 module.exports = mongoose.model('Log', schema);

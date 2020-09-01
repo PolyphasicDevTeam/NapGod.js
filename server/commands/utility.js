1d62e9de9baf4210f2e2acc96be5aa4fa68f0e7c
Jackred <jackred@tuta.io>
2020-09-01 23:46:54 +0100

    UPDATE eror commands to send

--- server/commands/utility.js
+++ server/commands/utility.js
@@ -1,3 +1,6 @@
+const config = '../../config.json';
+
+const { RichEmbed } = require('discord.js');
 function cutAt(string, nbMax, limitChar = '') {
   if (string.length > nbMax) {
     string = string.slice(0, nbMax);
@@ -9,6 +12,38 @@ function cutAt(string, nbMax, limitChar = '') {
   return string;
 }
 
+function buildEmbedError(error) {
+  let embed = new RichEmbed();
+  embed.setTitle(error.name);
+  embed.setDescription(error.stack);
+  embed.setColor('#FF0000');
+  embed.setTimestamp();
+  return embed;
+}
+
+async function sendError(error, message, dry = false) {
+  console.log(error);
+  if (!dry) {
+    let channel = message.guild.channels.get(config.logErrorChannelID);
+    if (channel === undefined) {
+      channel = message.channel;
+      await message.channel.send('Log channel not found. Dumping error here');
+    }
+    await channel.send(buildEmbedError(error, channel));
+  }
+}
+
+async function executeFunction(fn, args, message, dry = false) {
+  try {
+    console.log(fn);
+    await fn(args, message, dry);
+  } catch (e) {
+    console.log('?');
+    await sendError(e, message, dry);
+  }
+}
+
 module.exports = {
   cutAt,
+  executeFunction,
 };

d71dd32bed5dc369a280b4643d9e6c3e7acc179a
Jackred <jackred@tuta.io>
2020-09-10 11:26:50 +0100

    UPDATE indentation mset files

--- server/commands/mset.command.js
+++ server/commands/mset.command.js
@@ -1,35 +1,35 @@
-const { URL } = require("url");
-const _ = require("lodash");
-const UserModel = require("./../models/user.model");
-const { getOrGenImg, makeNapChartImageUrl } = require("./../imageCache");
-const config = require("../../config.json");
-const mset = require("./mset.backend").msetInternal;
+const { URL } = require('url');
+const _ = require('lodash');
+const UserModel = require('./../models/user.model');
+const { getOrGenImg, makeNapChartImageUrl } = require('./../imageCache');
+const config = require('../../config.json');
+const mset = require('./mset.backend').msetInternal;
 
 module.exports = {
-	processMset: function(command, message, args, dry=false) {
-		if (command === "mset") {
-			let roles =  message.member.roles
-			roles = new Set(roles.keys())
-			let mods = message.guild.roles.find("name", "Moderator").id
-			let admins = message.guild.roles.find("name", "Admins").id
-			permissions = false
-			if (roles.has(mods)||roles.has(admins)) {
-				permissions = true
-			}
-			if (!permissions) {
-				msg = "You do not have privileges to execute this commands. Only Moderators and Admins are allowed to use `+mset`"
-				console.log("MSG   : ", msg)
-				if(!dry){message.channel.send(msg);}
-			}
-			else if (args.length >= 3) {
-				mset(args, message, dry);
-			} else {
-				msg = "Valid options are `+mset [schedule-name] [napchart-link] [username]`"
-				console.log("MSG   : ", msg)
-				if(!dry){message.channel.send(msg);}
-			}
-			return true
-		}
-		return false
-	}
+  processMset: function (command, message, args, dry = false) {
+    if (command === 'mset') {
+      const permissions = message.member.roles.some((d) =>
+        ['Admins', 'Moderators'].includes(d.name)
+      );
+      if (!permissions) {
+        const msg =
+          'You do not have privileges to execute this commands. Only Moderators and Admins are allowed to use `+mset`';
+        console.log('MSG   : ', msg);
+        if (!dry) {
+          message.channel.send(msg);
+        }
+      } else if (args.length >= 3) {
+        mset(args, message, dry);
+      } else {
+        const msg =
+          'Valid options are `+mset [schedule-name] [napchart-link] [username]`';
+        console.log('MSG   : ', msg);
+        if (!dry) {
+          message.channel.send(msg);
+        }
+      }
+      return true;
+    }
+    return false;
+  },
 };

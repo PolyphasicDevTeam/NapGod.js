0860310fd93bacc5e2bed68f3549ad709491b67d
shaoyu <shaoyu0209@tuta.io>
2020-11-08 04:02:17 +0800

    Add new commands: SETTZ, GETTZ, STATUS

---
+++ server/commands/gettz.command.js
@@ -0,0 +1,69 @@
+const { URL } = require('url');
+const _ = require('lodash');
+const UserModel = require('./../models/user.model');
+const { getOrGenImg, makeNapChartImageUrl } = require('./../imageCache');
+const config = require('../../config.json');
+const { findMember } = require('./find');
+const { executeFunction, dateToStringSimple } = require('./utility');
+
+module.exports = {
+  processGet: function (command, message, args, dry = false) {
+    if (command === 'gettz') {
+      console.log('GETTZ', args);
+      executeFunction(get, message, args, dry);
+      return true;
+    }
+    return false;
+  },
+};
+
+function pad(number) {
+  if (number<=99) { number = ("0"+number).slice(-2); }
+  return number;
+}
+async function get(message, args, dry) {
+  const memberIdentifier = message.content
+    .slice(config.prefix.length + 5, message.content.length)
+    .trim();
+  console.log('INFO:  memberIdentifier: ', memberIdentifier);
+  let member;
+  if (memberIdentifier === '') {
+    member = { value: message.member, found: true };
+    console.log(
+      `INFO:  user is the author message ${member.value.user.tag} -> ${member.value.id}`
+    );
+  } else {
+    member = findMember(
+      memberIdentifier,
+      message.guild,
+      message.mentions.users
+    );
+    if (!member.found) {
+      console.log(member.msg);
+      if (!dry) {
+        await message.channel.send(member.msg);
+      }
+    } else {
+      console.log(
+        `INFO:  user found ${member.value.user.tag} -> ${member.value.id}`
+      );
+    }
+  }
+  if (member.found) {
+    const userDB = await UserModel.findOne({ id: member.value.user.id });
+    tzmin = userDB.timezone;
+    sign = Math.sign(tzmin);
+    if (sign<0){
+      sign = "-";
+    }
+    else{
+      sign = "+";
+    }
+    tzmin = Math.abs(tzmin);
+    hours = Math.floor(tzmin/60);
+    minutes = tzmin - hours*60;
+    message.channel.send("Timezone for " +
+      member.value.displayName + " is `"
+      + "UTC" + sign + pad(hours) + ":" + pad(minutes) + "`");
+  }
+}

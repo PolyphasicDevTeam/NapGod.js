71a577dc13394d671064f4916b2cf12ac42f0f94
Corentin Giroud-Argoud <corentin.giroud.argoud@ecomail.fr>
2020-12-11 22:01:33 +0100

    refactor(hist): histadapt to use adaptDate

--- server/commands/histadapt.command.js
+++ server/commands/histadapt.command.js
@@ -4,6 +4,7 @@ const { findMember } = require('./find');
 const schedules = require("./schedules").schedules;
 const modifiers = require("./schedules").modifiers;
 const { tick, bold, dateToStringSimple, isValidDate } = require('./utility');
+const { prefix } = require("../../config.json");
 
 const cmds = ["histadapt", "historyadapt", "histadapted", "historyadapted"];
 
@@ -17,11 +18,12 @@ module.exports = {
         message.channel.send('You do not have privileges to execute this command. Only Moderators and Admins are allowed to use `+histadapt`');
       }
       else if (args.length == 3) {
-        adapt(message, args, dry, cmd = command);
+        adapt(message, args, cmd = command);
         return true;
-    }
+      }
       else {
-        message.channel.send("Correct usage: `+histadapt [index] [yyyy-mm-dd] [user]`")
+        message.channel.send("Correct usage: "
+          + tick(prefix + "histadapt [index] [yyyy-mm-dd] [user]"));
       }
   }
   else {
@@ -30,7 +32,7 @@ module.exports = {
 }
 };
 
-async function adapt(message, args, dry, cmd) {
+async function adapt(message, args, cmd) {
   let index = args[0];
   let date = args[1]
   let memberIdentifier = args.slice(2).join(" ").trim();
@@ -47,9 +49,7 @@ async function adapt(message, args, dry, cmd) {
     console.log('INFO:  memberIdentifier: ', memberIdentifier);
     if (!member.found) {
       console.log(member.msg);
-      if (!dry) {
-        await message.channel.send(member.msg);
-      }
+      message.channel.send(member.msg);
       return;
     } else {
       console.log(`INFO:  user found ${member.value.user.tag} -> ${member.value.id}`);
@@ -63,70 +63,35 @@ async function adapt(message, args, dry, cmd) {
     return;
   }
 
-  let item = {
-    name: history[index].name,
-    adapted: true,
-    setAt: new Date(Date.parse(date))
-  };
-
-  let scheduleMatch = (sched) => (sched.name == item.name && sched.setAt == item.setAt && sched.adapted == item.adapted);
-  if (!checkIsValid(item, index, history, message)) {
+  let adaptDate = new Date(Date.parse(date));
+  if (!checkIsValid(adaptDate, index, history, message)) {
     return;
   }
 
   console.log("CMD   : HISTADAPT");
   console.log("ARGS  : ", args);
 
-  let userUpdate = {
-    tag: member.value.user.tag,
-    userName: member.value.user.username,
-    historicSchedules: history
-  };
+  history[index].adaptDate = adaptDate;
+  userDB.save();
 
-  let result = await saveUserHistory(message, userUpdate);
-
-  async function saveUserHistory(message, userUpdate) {
-    let query = { id: member.value.user.id };
-    let options = { upsert: true, new: true, setDefaultsOnInsert: true };
-
-    let result = null;
-    try {
-      result = await UserModel.findOneAndUpdate(query, userUpdate, options);
-    } catch (error) {
-      console.log("error searching for User: ", error);
-      if (!dry&&!silent) {
-        message.channel.send("Something broke.  Call the fire brigade");
-      }
-      return;
-    }
-    message.channel.send("Schedule history for " + bold(member.value.displayName) + " has been edited.\n" + "Adapted: " +
-      tick((parseInt(index) + 1 ) + " " + item.name + " " + dateToStringSimple(new Date(item.setAt)).slice(0,10)));
-
-    return result;
-
-  }
+  message.channel.send(
+    "Schedule history for " + bold(member.value.displayName)
+    + " has been edited.\n" + "Adapted: " + tick((parseInt(index) + 1 ) + " "
+    + item.name + " " + dateToStringSimple(new Date(item.setAt)).slice(0,10))
+  );
 }
 
-function checkIsValid (item, index, history, message) {
-
-  history.push(item);
+function checkIsValid(adaptDate, index, history, message) {
   history.sort((a,b) => (a.setAt > b.setAt) ? 1 : -1);
-  let scheduleMatch = (sched) => (sched.name == item.name && sched.setAt == item.setAt);
-  let newIndex = history.findIndex(scheduleMatch);
 
   // can't be adapted in the future
-  if (item.setAt > new Date()){
+  if (adaptDate > new Date()){
     message.channel.send("Adapted cannot be set in the future.")
     return false;
   }
 
-  if (newIndex != parseInt(index) + 1) {
-    console.log(newIndex, index);
-    message.channel.send("There are intervening schedules. Please delete them with `+histdel` before proceeding.");
-    return false;
-  }
   // can't set adapted before schedule start date
-  if (item.setAt < history[index].setAt) {
+  if (adaptDate < history[index].setAt) {
     message.channel.send("Adapted cannot be set before schedule start date.")
     return false;
   }

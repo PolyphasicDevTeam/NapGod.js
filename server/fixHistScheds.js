355c6df00c0c55cd7832d7c72209b76ac7d570b8
Corentin Giroud-Argoud <corentin.giroud.argoud@ecomail.fr>
2020-12-11 21:59:11 +0100

    fix(fixDb): link to userId, check userName using regex, merge logs

--- server/fixHistScheds.js
+++ server/fixHistScheds.js
@@ -24,7 +24,7 @@ async function fixLogModels(users) {
   for (const user of users) {
     let userLogs = null;
     try {
-      userLogs = await LogModel.find({userName: user.userName});
+      userLogs = await LogModel.find({userName: {$regex : '^' + user.userName + '( [.*])?$'}});
     }
     catch(err) {
       console.error(err);
@@ -38,12 +38,16 @@ async function fixLogModels(users) {
       console.log(`INFO: No log found for ${user.userName}`);
     }
     userLogs.forEach(userLog => {
+      if (userLog.historicId) {
+        return;
+      }
       let historicId = user.historicSchedules
         .filter((s) => s.name === userLog.schedule)
         .slice(-userLog.attempt)[0]
         ._id;
       console.log(`INFO: Setting id ${historicId} to ${userLog.schedule}` +
         ` attempt #${userLog.attempt} for ${user.userName}`);
+      userLog.userId = user.id;
       userLog.historicId = historicId;
       userLog.save();
     });
@@ -51,7 +55,7 @@ async function fixLogModels(users) {
 }
 
 async function fixHistoric(users) {
-  users.forEach(user => {
+  users.forEach(async (user) => {
     console.log(`INFO: Fixing history for user ${user.userName}`);
     for (let i = user.historicSchedules.length - 1; i; i--) {
       if (!user.historicSchedules[i].adaptDate &&
@@ -59,6 +63,31 @@ async function fixHistoric(users) {
         user.historicSchedules[i].adapted && !user.historicSchedules[i - 1].adapted)
       {
         console.log(`INFO: Merging ${user.historicSchedules[i].name} at index ${i} with index ${i-1}`);
+        let adaptLogs = null;
+        let preAdaptLogs = null;
+        try {
+          adaptLogs = await LogModel.findOne({userName: user.userName,
+            historicId: user.historicSchedules[i]._id});
+          preAdaptLogs = await LogModel.findOne({userName: user.userName,
+            historicId: user.historicSchedules[i - 1]._id});
+        }
+        catch(err) {
+          console.error(`Error fetching logmodels when merging: ${err}`);
+          return;
+        }
+        if (adaptLogs && preAdaptLogs) {
+          console.log(preAdaptLogs.entries);
+          preAdaptLogs.entries.concat(adaptLogs.entries);
+          preAdaptLogs.save();
+          try {
+            await LogMode.deleteOne({userName: user.userName,
+              historicId: user.historicSchedules[i]._id});
+          }
+          catch(err) {
+            console.err(`Error deleting adaptlog model: ${err}`);
+            return;
+          }
+        }
         user.historicSchedules[i - 1].adaptDate = user.historicSchedules[i].setAt;
         user.historicSchedules.splice(i, 1);
         i--;
